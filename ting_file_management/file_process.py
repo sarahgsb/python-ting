271a19f5135b79493d7c85ab3ba10bfb8fe9b37e
Sarah <sarahgsb@hotmail.com>
2022-04-05 13:46:37 -0300

    3, 4, 5 - file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,29 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+
+    if path_file not in [index["nome_do_arquivo"] for index in instance._data]:
+        instance.enqueue(data)
+        sys.stdout.write(str(data))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance.__len__():
+        return sys.stdout.write("Não há elementos\n")
+
+    data = instance.dequeue()["nome_do_arquivo"]
+    return sys.stdout.write(f"Arquivo {data} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return sys.stdout.write(str(instance.search(position)))
+    except IndexError:
+        return sys.stderr.write("Posição inválida\n")
