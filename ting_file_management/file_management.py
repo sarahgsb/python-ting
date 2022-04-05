35b71b3a99c9937e593e034a59570981c559d87e
Sarah <sarahgsb@hotmail.com>
2022-04-05 09:35:32 -0300

    2 - file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                lines = file.read().splitlines()
+                return lines
+
+        except FileNotFoundError:
+            return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    return sys.stderr.write("Formato inválido\n")
